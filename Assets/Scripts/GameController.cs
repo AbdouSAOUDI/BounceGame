def7db0ae3854b51c62f7a991995d95e1896cc69
Star-PRO-X <Abderrahmane.saoudi.26@gmail.com>
2019-05-17 08:38:41 +0100

    Version 0.3
    this version had a problem with player jumping...

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    private bool p = false;
+    public void Pause()
+    {
+        if (p)
+        {
+            Time.timeScale = 0f;
+            p = true;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            p = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D player;
+    [SerializeField] private Animator anim;
+    [SerializeField] private float horisontalForce, speedJump;
+    [SerializeField] private bool b = false;
+    public float SpeedJump = 50f;
+    private void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+        anim.GetComponent<Animator>();
+    }
+    void FixedUpdate()
+    {
+        moveHorizontal (Input.GetAxisRaw("Horizontal"));
+        //if (Input.GetKey(KeyCode.Space) && b) player.AddForce(verticalForce * Time.deltaTime);
+        if (b)
+        {
+            player.velocity = player.transform.up * speedJump;
+            b = false;
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Input.mousePosition.x > Screen.width / 2)
+            {
+                //Move Player Left
+
+                player.velocity = Vector2.up * speedJump * Input.GetAxis("Jump");
+                //extrajumps--;
+                //anim.SetBool("isJumping", true);
+            }
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground") b = true;
+        if (collision.gameObject.tag == "Enemy") anim.SetBool("Death", true);
+        anim.SetBool("Death", false);
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground") b = true;
+    }
+    public void moveHorizontal(float value)
+    {
+        //player.AddForce(value * horisontalForce * Time.deltaTime, 0f);
+        Vector2 v2;
+        v2.x = horisontalForce;
+        v2.y = 0f;
+        player.AddForce(value * v2 * Time.deltaTime);
+    }
+    public void moveVertical()
+    {
+        player.velocity = player.transform.up * speedJump * Time.deltaTime;
+    }
+}
